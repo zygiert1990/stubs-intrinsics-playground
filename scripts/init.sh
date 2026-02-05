262ab53451f6cda11e58176b67af1186f9ab96ea
Michał Zyga <michal.zyga@softwaremill.com>
2026-02-05 07:38:56 +0100

    bump java & scala-cli versions

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,10 +4,10 @@ apt install zip unzip -y
 ## Install sdkman & tools
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 25.0.1-tem
-sdk install scalacli 1.12.0
+sdk install java 25.0.2-tem
+sdk install scalacli 1.12.1
 source ~/.bashrc
 echo 'JAVA_HOME'
 echo $JAVA_HOME
 ## Get hsdis library
-wget "https://chriswhocodes.com/hsdis/hsdis-amd64.so" -P "$HOME/.sdkman/candidates/java/25.0.1-tem/lib/server/"
+wget "https://chriswhocodes.com/hsdis/hsdis-amd64.so" -P "$HOME/.sdkman/candidates/java/25.0.2-tem/lib/server/"
