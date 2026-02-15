acbc1838021412feeb5628b084d471d293e4de6e
Michał Zyga <michal.zyga@softwaremill.com>
2026-02-15 07:48:29 +0100

    go back to Main with pow

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -3,7 +3,15 @@ package playground;
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Power calculation finished: " + power());
+    }
+
+    private static long power() {
+        var result = 0L;
+        for (int i = 0; i < 1_000_000; i++) {
+            result += (long) Math.pow(i, 2.0);
+        }
+        return result;
     }
 
 }
