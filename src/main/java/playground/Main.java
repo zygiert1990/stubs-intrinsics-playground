8fae56e1b49538794816090fa271b3da2223ce13
Michał Zyga <36279820+zygiert1990@users.noreply.github.com>
2026-02-15 07:05:01 +0100

    Update Main.java

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -3,15 +3,7 @@ package playground;
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Power calculation finished: " + power());
-    }
-
-    private static long power() {
-        var result = 0L;
-        for (int i = 0; i < 1_000_000; i++) {
-            result += (long) Math.pow(i, 2.0);
-        }
-        return result;
+        System.out.println("Hello world!");
     }
 
 }
